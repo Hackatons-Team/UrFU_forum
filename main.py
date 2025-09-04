b480f8e46b4fd6a2fad54795029ef5b988e48e34
EmployeerFinalVersion <77688336+EmployeerFinalVersion@users.noreply.github.com>
2025-09-04 23:18:56 +0500

    Отображение ответов пользователя

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ with open('key.txt', 'r') as f:
     bot = telebot.TeleBot(f.readline())
 
 previousMessage = {}
+previousQuestion = {}
 accesibleMessages = ["Введите вопрос", "Напиши то, что помогло тебе или поможет абитуриенту"]
 
 categories = None
@@ -24,24 +25,34 @@ questions = None
 with open('questions.json', 'r', encoding="UTF-8") as f:
     questions = json.load(f)
 
+def send_question(topic, chatID=None):
+    bot.send_message(chatID, f"Тема существующего вопроса: {topic}", reply_markup=create_menu(previousMessage[chatID]))
+
+
 
 def get_data(parentID: str, whatFind: str = "category", whereFind: str = "category", chatID: str = "") -> list: # Проверка ответных сообщений происходит тут
     result = []
     if whereFind == "category": # Выбор категории
         if parentID in precalculatedCategories:
             for c in precalculatedCategories[parentID]: # [ { (, ) }, {...}]
-                if whatFind == "category":
+                if "itsquestion" in c:
+                    send_question(c[2], chatID=chatID) # (..., ..., topic)
+                    return get_data(parentID, whatFind="question", whereFind="question", chatID=chatID)
+                elif whatFind == "category":
                     result.append(c[1]) # (..., name)
         else:
             result.append("Назад")
         return result.copy()
-    elif whereFind == "question": # Выбор вопроса
+    elif whereFind == "question": # Выбор вопроса get_data(smID, "question", "question")
+        result.append("itsquestion")
         if parentID in precalculatedQuestions:
-            for q in precalculatedQuestions[parentID]: # [ { ... }, { ... }]
+            for idx in range(len(precalculatedQuestions[parentID])): # [ { ... }, { ... }]
+                q = precalculatedQuestions[parentID][idx]
                 if whatFind == "question":
                     result.append({"topic": q[2], "text": q[1]}) # (..., text, topic)
-                elif whatFind == "answer": # Ответ пользователя
-                    result.append({"text": q[1]}) # (..., text)
+                    previousQuestion[chatID] = q[2]
+                elif whatFind == "answer": # Ответ пользовател        
+                    result.append(q[1]) # (..., text)
         return result.copy()
     return
 
@@ -52,16 +63,34 @@ def take_data(data: str, chatID: str = ""):
         if previousMessage[chatID] == "Введите вопрос": # Создание вопроса
             # Отправление вопроса ИИ
             # Сохранение
-            # data = {"ID": max(n)+1,
-            #         "topic": some text 1,
-            #         "text": some text 2,
-            #         "parentID": const,
-            #         "type": question
-            #        }
-            # with open("questions.json", "w") as f:
-            #     json.dump(data, f, indent=4)
+            maxID = 0
+            for i in categories:
+                if int(i["ID"]) > maxID:
+                    maxID = int(i["ID"])
+            local_data = {
+                "ID": maxID+1,
+                "topic": data,
+                "text": "",
+                "parentID": 9,
+                "type": "question"
+                }
+            '''with open("questions.json", "w+") as f:
+                someunuseful = json.load(f)
+                print(len(someunuseful))
+                someunuseful.append(local_data)
+                print(len(someunuseful))
+                json.dump(someunuseful, f, indent=4)
+
+            local_data['name'] = local_data['topic']
+
+            with open("categories.json", "w+") as f:
+                someunuseful = json.load(f)
+                print(len(someunuseful))
+                someunuseful.append(local_data)
+                print(len(someunuseful))
+                json.dump(local_data, f, indent=4)'''
             print("Вопрос:", end=" ")
-            bot.send_message(chatID, "Вопрос записан", reply_markup=back)
+            bot.send_message(chatID, "Данные сохранены", reply_markup=back)
         elif previousMessage[chatID] == "Городские": # Переход в городское общежитие
             # Поиск узла с общагой указанного номера
             pass
@@ -76,9 +105,18 @@ def take_data(data: str, chatID: str = ""):
     # Сохранение
 
 
-def create_buttons(parentID) -> tuple:
+
+def create_buttons(parentID, chatID=None) -> tuple:
     #return tuple(types.InlineKeyboardButton(text=name, callback_data=str(name)) for name in get_data(parentID, "category"))
-    return tuple(types.KeyboardButton(name) for name in get_data(parentID, "category"))
+    data = get_data(parentID, "category", chatID=chatID)
+    if data[0] != "itsquestion":
+        result = []
+        for name in data:
+            result.append(types.KeyboardButton(name))
+        return tuple(result)
+    else:
+        #data = data[1:]
+        return "создать кнопки вопросов"
 
 
 
@@ -96,7 +134,7 @@ precalculatedCategories={}
 # }
 precalculatedQuestions={}
 # precalculatedQuestions = {
-#   "100": [
+#   "100": [ 
 #           (
 #               "ID": 100,
 #               "topic": "Название вопроса",
@@ -127,11 +165,15 @@ for node in categories: # Формирование дерева категори
         if not nextId in precalculatedCategories:
             if "answer" in node:
                 precalculatedCategories[nextId] = [(node["ID"], node["name"], node["answer"])]
+            elif "topic" in node:
+                precalculatedCategories[nextId] = [(node["ID"], node["name"], node["topic"], "itsquestion")]
             else: 
                 precalculatedCategories[nextId] = [(node["ID"], node["name"])]
         else:
             if "answer" in node:
                 precalculatedCategories[nextId].append((node["ID"], node["name"], node["answer"]))
+            elif "topic" in node:
+                precalculatedCategories[nextId].append((node["ID"], node["name"], node["topic"], "itsquestion"))
             else:
                 precalculatedCategories[nextId].append((node["ID"], node["name"]))
             
@@ -155,14 +197,25 @@ for node in questions: # Формирование дерева вопросов
                 precalculatedQuestions[nextId] = [(node["ID"], node["text"])]
         else:
             if node["type"] == "question":
-                precalculatedQuestions[nextId].append([(node["ID"], node["text"], node["topic"])])
+                precalculatedQuestions[nextId].append((node["ID"], node["text"], node["topic"]))
             elif node["type"] == "answer":
-                precalculatedQuestions[nextId].append([(node["ID"], node["text"])])
+                precalculatedQuestions[nextId].append((node["ID"], node["text"]))
 
-def create_menu(id: str):
+
+def createQuestionButtons():
+    return [types.KeyboardButton("Просмотреть ответы"),
+            types.KeyboardButton("Написать ответ")]
+
+def create_menu(id: str, chatID=None):
     menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    for smth in create_buttons(id):
-        menu.add(smth)
+    tmp_res = create_buttons(id, chatID=chatID)
+    if tmp_res != "создать кнопки вопросов":
+        for smth in create_buttons(id, chatID=chatID):
+            menu.add(smth)
+    else:
+        for smth in createQuestionButtons():
+            menu.add(smth)
+        return (menu, "Можете как добавить ответы, так и просмотреть существующие")
     return menu
 
 back = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -187,17 +240,20 @@ def answer_to_message(messageText, chatID = None):
                     return (answerText, back)
                 else:
                     idToFind = str(node[0])
-                    menu = create_menu(idToFind)
+                    menu = create_menu(idToFind, chatID=chatID)
 
                     if len(node) > 2:
                         answerText = node[2]
+                    try:
+                        if len(menu) > 1:
+                            answerText = menu[1]
+                            menu = menu[0]
+                    except:
+                        pass
+                    
 
                     return (answerText, menu)
     return (answerText, menu)
-
-"""@bot.callback_query_handler(func=lambda call: True)
-def handle_callback(call):
-    text_messages(call, isInlinePressed = True)"""
 
 
 @bot.message_handler(content_types=["text"])
@@ -214,6 +270,22 @@ def text_messages(message, isInlinePressed = False):
         
     if not idOfChat in previousMessage:
         previousMessage[idOfChat] = ""
+
+    if not idOfChat in previousQuestion:
+        previousQuestion[idOfChat] = ""
+        
+    if textOfMessage == "Просмотреть ответы":
+        # Найти Ид вопроса
+        idToPaste = None
+        for ww in precalculatedQuestions:
+            if previousQuestion[idOfChat] in precalculatedQuestions[ww][0]: # Если это тот вопрос
+                idToPaste = str(precalculatedQuestions[ww][0][0])
+        t = get_data(idToPaste, whatFind = "answer", whereFind = "question")
+        for mes in t[1:]:
+            bot.send_message(idOfChat, mes)
+        bot.send_message(idOfChat, "Конец ответов", reply_markup=back)
+        return
+
     
     if previousMessage[idOfChat] in accesibleMessages:
         take_data(textOfMessage, idOfChat) # Обработка занесения данных
